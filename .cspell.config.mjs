9e6bbbd1077a1c58be1fa2f84615f876d125bb88
Jonathan Garbee <jonathan@garbee.me>
2026-02-07 20:27:55 -0500

    chore(cspell): setup and configure cpsell

---
+++ .cspell.config.mjs
@@ -0,0 +1,27 @@
+export default {
+    // Version of the setting file.  Always 0.2
+    "version": "0.2",
+    "language": "en_US",
+    "dictionaryDefinitions": [
+        {
+            "name": "project-words",
+            "path": "./etc/cspell/words.txt"
+        },
+        {
+            "name": "third-party-names",
+            "path": "./etc/cspell/third-party-names.txt"
+        }
+    ],
+    "dictionaries": [
+        "bash",
+        "typescript",
+        "node",
+        "go",
+        "filetypes",
+        "companies",
+        "softwareTerms",
+        "misc",
+        "project-words",
+        "third-party-names"
+    ]
+}
