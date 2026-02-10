a119132164812e916fa389c1e4dfe71274bc09f2
Jonathan Garbee <jonathan@garbee.me>
2026-02-09 21:24:35 -0500

    chore: prepare for cspell commit check

--- .cspell.config.mjs
+++ .cspell.config.mjs
@@ -1,27 +1,33 @@
+const dictionaryDefinitions = [
+    {
+        "name": "project-words",
+        "path": "./etc/cspell/words.txt"
+    },
+    {
+        "name": "third-party-names",
+        "path": "./etc/cspell/third-party-names.txt"
+    }
+];
+
 export default {
     // Version of the setting file.  Always 0.2
     "version": "0.2",
     "language": "en_US",
-    "dictionaryDefinitions": [
-        {
-            "name": "project-words",
-            "path": "./etc/cspell/words.txt"
-        },
-        {
-            "name": "third-party-names",
-            "path": "./etc/cspell/third-party-names.txt"
-        }
-    ],
+    "dictionaryDefinitions": dictionaryDefinitions,
     "dictionaries": [
         "bash",
-        "typescript",
-        "node",
-        "go",
-        "filetypes",
         "companies",
-        "softwareTerms",
+        "docker",
+        "filetypes",
+        "git",
+        "golang",
         "misc",
-        "project-words",
-        "third-party-names"
+        "node",
+        "shell",
+        "software-terms",
+        "softwareTerms",
+        "typescript",
+
+        ...dictionaryDefinitions.map(def => def.name)
     ]
 }
